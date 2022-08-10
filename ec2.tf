a7f349d0949a7ff36e63bca0ffa2831534a7697b
Ravi <ravi@gmail.com>
2022-08-10 15:52:23 +0530

    ec2 code

--- ec2.tf
+++ ec2.tf
@@ -8,7 +8,24 @@ resource "aws_instance" "instance" {
   }
 }
 
+resource "null_resource" "copy-local-artifact" {
+  provisioner "file" {
+    connection {
+      user     = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_USER"]
+      password = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_PASS"]
+      host     = aws_instance.instance.private_ip
+    }
+
+    provisioner "file" {
+      source      = "${var.COMPONENT}-${var.APP_VERSION}.zip"
+      destination = "/tmp/{{ COMPONENT }}.zip"
+    }
+
+  }
+}
+
 resource "null_resource" "ansible" {
+  depends_on = [null_resource.copy-local-artifact]
   provisioner "remote-exec" {
     connection {
       user     = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_USER"]
@@ -18,6 +35,5 @@ resource "null_resource" "ansible" {
     inline = [
       "ansible-pull -U https://github.com/devopsravi9/roboshop-ansible.git roboshop.yml -e HOST=localhost -e ROLE=${var.COMPONENT} -e ENV=ENV -e DOCDB_ENDPOINT=DOCDB_ENDPOINT -e REDDIS_ENDPOINT=REDDIS_ENDPOINT -e MYSQL_ENDPOINT=MYSQL_ENDPOINT"
     ]
-
   }
 }
